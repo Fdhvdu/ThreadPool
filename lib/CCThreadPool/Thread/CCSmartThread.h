c2772d31e514975a5e1a7b82ad1e333a3dafb6d2
Fdhvdu <fdhvducipher@gmail.com>
2015-12-16 17:42:55 +0800

    add move constructor

--- lib/CCThreadPool/Thread/CCSmartThread.h
+++ lib/CCThreadPool/Thread/CCSmartThread.h
@@ -12,6 +12,7 @@ namespace nTool
 		template<class Func,class ... Args>
 		explicit CSmartThread(Func &&,Args &&...);
 		CSmartThread(const CSmartThread &)=delete;
+		CSmartThread(CSmartThread &&) noexcept=default;
 		CSmartThread& operator=(const CSmartThread &)=delete;
 		CSmartThread& operator=(CSmartThread &&) noexcept=default;
 		~CSmartThread();
