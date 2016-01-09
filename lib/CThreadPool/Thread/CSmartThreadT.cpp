45a40f4fb4f718d2867d7c0ceb2f5fb68f2a3e65
Fdhvdu <fdhvducipher@gmail.com>
2016-01-09 21:41:26 +0800

    move to another repository (lib)

--- lib/CThreadPool/Thread/CSmartThreadT.cpp
+++
@@ -1,9 +0,0 @@
-#include"CSmartThread.h"
-#include<utility>
-
-namespace nTool
-{
-	template<class Func,class ... Args>
-	CSmartThread::CSmartThread(Func &&func,Args &&... args)
-		:thr_{std::forward<Func>(func),std::forward<Args>(args)...}{}
-}
--- lib/CThreadPool/Thread/Tool/CId.cpp
+++
@@ -1,7 +0,0 @@
-#include"CID.h"
-
-namespace nTool
-{
-	CId::CId(const std::size_t init) noexcept
-		:id_{init}{}
-}
