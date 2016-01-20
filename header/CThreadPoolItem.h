b129e13940084546c0d9e095c8400974a5b737de
Fdhvdu <fdhvducipher@gmail.com>
2016-01-20 17:49:57 +0800

    hide IThreadPoolItemExecutorBase

--- header/CThreadPoolItem.h
+++ header/CThreadPoolItem.h
@@ -1,16 +1,12 @@
 #ifndef CTHREADPOOLITEM
 #define CTHREADPOOLITEM
-#include<atomic>	//atomic<bool>
 #include<functional>	//function
 #include<memory>	//unique_ptr
-#include"../../lib/header/thread/CSemaphore.h"
 #include"../../lib/header/tool/CPimpl.h"
 #include"CThreadPoolCommun.h"
 
 namespace nThread
 {
-	struct IThreadPoolItemExecutorBase;
-
 	class CThreadPoolItem
 	{
 		struct Impl;
@@ -26,67 +22,6 @@ namespace nThread
 		void setCommun(std::unique_ptr<CThreadPoolCommunBase> &&);
 		CThreadPoolItem& operator=(const CThreadPoolItem &)=delete;
 		~CThreadPoolItem();
-	};
-
-	struct IThreadPoolItemExecutorBase	//I give up to use Non-Virtual Interface Idiom here
-										//because this is a abstract base struct
-	{
-		virtual void exec()=0;
-		virtual bool is_running() const noexcept=0;
-		bool joinable() const noexcept
-		{
-			return joinable_();
-		}
-		virtual void wait()=0;
-		virtual ~IThreadPoolItemExecutorBase()=0;
-	protected:
-		virtual bool joinable_() const noexcept
-		{
-			return false;
-		}
-	};
-
-	class CThreadPoolItemExecutorDetach:public IThreadPoolItemExecutorBase
-	{
-		CThreadPoolCommunBase *commun_;
-		CSemaphore complete_;
-		std::function<void()> func_;
-	public:
-		CThreadPoolItemExecutorDetach(CThreadPoolCommunBase *,std::function<void()> &&);
-		CThreadPoolItemExecutorDetach(const CThreadPoolItemExecutorDetach &)=delete;
-		void exec() override;
-		bool is_running() const noexcept override	//only the destructor of CThreadPoolItem will call this
-		{
-			return !complete_.count();
-		}
-		void wait() override	//only the destructor of CThreadPoolItem will call this
-		{
-			complete_.wait();
-		}
-		CThreadPoolItemExecutorDetach& operator=(const CThreadPoolItemExecutorDetach &)=delete;
-	};
-
-	class CThreadPoolItemExecutorJoin:public IThreadPoolItemExecutorBase
-	{
-		CThreadPoolCommunBase *commun_;
-		CSemaphore complete_;
-		std::function<void()> func_;
-		std::atomic<bool> running_;
-	protected:
-		bool joinable_() const noexcept override
-		{
-			return true;
-		}
-	public:
-		CThreadPoolItemExecutorJoin(CThreadPoolCommunBase *,std::function<void()> &&);
-		CThreadPoolItemExecutorJoin(const CThreadPoolItemExecutorJoin &)=delete;
-		void exec() override;
-		bool is_running() const noexcept override
-		{
-			return running_;
-		}
-		void wait() override;
-		CThreadPoolItemExecutorJoin& operator=(const CThreadPoolItemExecutorJoin &)=delete;
 	};
 }
 
--- src/CThreadPoolItem.cpp
+++ src/CThreadPoolItem.cpp
@@ -1,10 +1,69 @@
 #include"../header/CThreadPoolItem.h"
+#include<atomic>	//atomic<bool>
 #include<utility>	//move
+#include"../../lib/header/thread/CSemaphore.h"
 #include"../../lib/header/thread/CSmartThread.h"
 using namespace std;
 
 namespace nThread
 {
+	struct IThreadPoolItemExecutorBase	//I give up to use Non-Virtual Interface Idiom here
+										//because this is a abstract base struct
+	{
+		virtual void exec()=0;
+		virtual bool is_running() const noexcept=0;
+		inline bool joinable() const noexcept
+		{
+			return joinable_();
+		}
+		virtual void wait()=0;
+		virtual ~IThreadPoolItemExecutorBase()=0;
+	protected:
+		virtual bool joinable_() const noexcept
+		{
+			return false;
+		}
+	};
+
+	class CThreadPoolItemExecutorDetach:public IThreadPoolItemExecutorBase
+	{
+		CThreadPoolCommunBase *commun_;
+		CSemaphore complete_;
+		function<void()> func_;
+	public:
+		CThreadPoolItemExecutorDetach(CThreadPoolCommunBase *,function<void()> &&);
+		void exec() override;
+		bool is_running() const noexcept override	//only the destructor of CThreadPoolItem will call this
+		{
+			return !complete_.count();
+		}
+		void wait() override	//only the destructor of CThreadPoolItem will call this
+		{
+			complete_.wait();
+		}
+	};
+
+	class CThreadPoolItemExecutorJoin:public IThreadPoolItemExecutorBase
+	{
+		CThreadPoolCommunBase *commun_;
+		CSemaphore complete_;
+		function<void()> func_;
+		atomic<bool> running_;
+	protected:
+		bool joinable_() const noexcept override
+		{
+			return true;
+		}
+	public:
+		CThreadPoolItemExecutorJoin(CThreadPoolCommunBase *,function<void()> &&);
+		void exec() override;
+		bool is_running() const noexcept override
+		{
+			return running_;
+		}
+		void wait() override;
+	};
+
 	struct CThreadPoolItem::Impl
 	{
 		unique_ptr<CThreadPoolCommunBase> commun;	//communicate with CThreadPool
@@ -26,6 +85,35 @@ namespace nThread
 		}
 		~Impl();
 	};
+
+	IThreadPoolItemExecutorBase::~IThreadPoolItemExecutorBase(){}
+
+	CThreadPoolItemExecutorDetach::CThreadPoolItemExecutorDetach(CThreadPoolCommunBase *commun,function<void()> &&func)
+		:commun_{commun},complete_{0},func_{move(func)}{}
+
+	void CThreadPoolItemExecutorDetach::exec()
+	{
+		func_();
+		complete_.signal();
+		commun_->communPoolDetach();
+	}
+
+	CThreadPoolItemExecutorJoin::CThreadPoolItemExecutorJoin(CThreadPoolCommunBase *commun,function<void()> &&func)
+		:commun_{commun},complete_{0},func_{move(func)},running_{true}{}
+
+	void CThreadPoolItemExecutorJoin::exec()
+	{
+		func_();
+		commun_->communPoolFinish();
+		complete_.signal();
+	}
+
+	void CThreadPoolItemExecutorJoin::wait()
+	{
+		complete_.wait();
+		running_=false;
+		commun_->communPoolJoin();
+	}
 
 	CThreadPoolItem::Impl::Impl()
 		:destructor{false},wait{0},thr{&CThreadPoolItem::Impl::loop,this}{}
@@ -85,33 +173,4 @@ namespace nThread
 	}
 
 	CThreadPoolItem::~CThreadPoolItem(){}
-
-	IThreadPoolItemExecutorBase::~IThreadPoolItemExecutorBase(){}
-
-	void CThreadPoolItemExecutorDetach::exec()
-	{
-		func_();
-		complete_.signal();
-		commun_->communPoolDetach();
-	}
-	
-	void CThreadPoolItemExecutorJoin::exec()
-	{
-		func_();
-		commun_->communPoolFinish();
-		complete_.signal();
-	}
-
-	void CThreadPoolItemExecutorJoin::wait()
-	{
-		complete_.wait();
-		running_=false;
-		commun_->communPoolJoin();
-	}
-
-	CThreadPoolItemExecutorDetach::CThreadPoolItemExecutorDetach(CThreadPoolCommunBase *commun,function<void()> &&func)
-		:commun_{commun},complete_{0},func_{move(func)}{}
-
-	CThreadPoolItemExecutorJoin::CThreadPoolItemExecutorJoin(CThreadPoolCommunBase *commun,function<void()> &&func)
-		:commun_{commun},complete_{0},func_{move(func)},running_{true}{}
 }
