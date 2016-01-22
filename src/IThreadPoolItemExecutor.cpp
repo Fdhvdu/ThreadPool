617f94aadfe6be1a01a7059cad0da9bd379ca9b3
Fdhvdu <fdhvducipher@gmail.com>
2016-01-22 19:36:19 +0800

    fix CThreadPoolItemExecutor

--- src/IThreadPoolItemExecutor.cpp
+++ src/IThreadPoolItemExecutor.cpp
@@ -1,4 +1,5 @@
 #include"../header/IThreadPoolItemExecutor.h"
+#include<atomic>	//atomic<bool>
 #include<utility>
 #include"../../lib/header/thread/CSemaphore.h"
 #include"../header/IThreadPoolCommun.h"
@@ -11,13 +12,14 @@ namespace nThread
 		unique_ptr<IThreadPoolCommunBase> commun;
 		CSemaphore complete;
 		function<void()> func;
+		atomic<bool> running;
 		Impl(unique_ptr<IThreadPoolCommunBase> &&,function<void()> &&);
 		void exec();
 		void wait();
 	};
 
 	CThreadPoolItemExecutor::Impl::Impl(unique_ptr<IThreadPoolCommunBase> &&commun_,function<void()> &&func_)
-		:commun{move(commun_)},complete{0},func{move(func_)}{}
+		:commun{move(commun_)},complete{0},func{move(func_)},running{true}{}
 
 	void CThreadPoolItemExecutor::Impl::exec()
 	{
@@ -29,6 +31,7 @@ namespace nThread
 	void CThreadPoolItemExecutor::Impl::wait()
 	{
 		complete.wait();
+		running=false;
 		commun->destroy();
 	}
 
@@ -44,7 +47,7 @@ namespace nThread
 	
 	bool CThreadPoolItemExecutor::is_running() const noexcept
 	{
-		return !impl_.get().complete.count();
+		return impl_.get().running;
 	}
 
 	void CThreadPoolItemExecutor::wait()
