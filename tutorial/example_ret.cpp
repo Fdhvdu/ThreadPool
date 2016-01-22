8c75ceb1641f7113f0acc539479f8cfa175ec737
Fdhvdu <fdhvducipher@gmail.com>
2016-01-22 22:08:45 +0800

    replace std::size_t with std::thread::id

--- tutorial/example_ret.cpp
+++ tutorial/example_ret.cpp
@@ -1,7 +1,9 @@
 #include<chrono>
 #include<iostream>
 #include<mutex>
+#include<queue>
 #include<random>
+#include<thread>
 #include"../header/CThreadPool_Ret.h"
 
 namespace
@@ -20,22 +22,24 @@ int main()
 {
 	using namespace std;
 	nThread::CThreadPool_Ret<size_t> tp{4};	//merely size_t, not size_t()
+	queue<nThread::CThreadPool_Ret<size_t>::thread_id> que;
 	for(size_t i{0};i!=tp.count();++i)
-		tp.add([&,i]{
+		que.emplace(tp.add([&,i]{
 			using namespace std;
 			const auto sec{get()};
 			this_thread::sleep_for(chrono::seconds{sec});
 			lock_guard<mutex> lock{mut};
 			cout<<"thread "<<i<<" wait "<<sec<<" sec"<<endl;
 			return i;
-		;});
-	tp.wait(0);	//tp will block here until thread[0] complete
-	cout<<tp.get(0)<<" complete"<<endl;	//get the return value from lambda
-	//tp.get(0);	//do not call same id twice
+		;}));
+	tp.wait(que.front());	//tp will block here until thread[que.front()] complete
+	cout<<tp.get(que.front())<<" complete"<<endl;	//get the return value from lambda
+	//tp.get(que.front());	//do not call same id twice
+	que.pop();
 
-	if(tp.valid(1))	//it means thread[1] has a function, and thread[1] is running
-		cout<<tp.get(1)<<" complete"<<endl;	//tp will block here until thread[1] complete and return value from lambda
-	if(tp.valid(1))	//this will return false, because you call tp.get(1)
+	if(tp.valid(que.front()))	//it means thread[que.front()] has a function, and thread[que.front()] is running
+		cout<<tp.get(que.front())<<" complete"<<endl;	//tp will block here until thread[que.front()] complete and return value from lambda
+	if(tp.valid(que.front()))	//this will return false, because you call tp.get(que.front())
 		cout<<"hi, something must wrong"<<endl;
 
 	//you don't need to call every get to guarantee all threads are not valid
