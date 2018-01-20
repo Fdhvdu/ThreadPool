0beb1750fd72d97d62cc2aa4ea10d28d26239e34
Fdhvdu <fdhvducipher@gmail.com>
2018-01-20 21:46:25 +0800

    add header and use C++11 using

--- performance comparison/header/test.hpp
+++ performance comparison/header/test.hpp
@@ -1,13 +1,14 @@
 #ifndef TEST
 #define TEST
 #include<thread>	//thread::hardware_concurrency
+#include<type_traits>
 #include"../../../lib/header/tool/CChrono_timer.hpp"
 
-typedef decltype(nTool::CChrono_timer().duration_nanoseconds()) duration;
+using duration=decltype(nTool::CChrono_timer().duration_nanoseconds());
 
-constexpr unsigned long iteration{10000000};
-constexpr std::result_of<decltype(std::thread::hardware_concurrency)&()>::type thread_count{12};
+constexpr unsigned long iteration(10000000);
+constexpr std::result_of_t<decltype(std::thread::hardware_concurrency)&()> thread_count(16);
 
-inline void empty(){};
+inline void empty() noexcept{}
 
 #endif
